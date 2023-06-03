38d77495f1bea540d0d33ef958e906af1008e259
Qwww Rere <qwertylollel@gmail.com>
2023-06-03 18:17:42 +0600

    start

---
+++ src/main/java/com/nuracell/shelltest/ShellTestApplication.java
@@ -0,0 +1,13 @@
+package com.nuracell.shelltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShellTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShellTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nuracell/shelltest/ShellTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuracell.shelltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShellTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
