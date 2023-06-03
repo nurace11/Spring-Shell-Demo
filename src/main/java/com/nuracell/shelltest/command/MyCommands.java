b177b8b35f102a55d1143e225fa3a8f441faf9a8
Qwww Rere <qwertylollel@gmail.com>
2023-06-03 18:49:38 +0600

    custom command

---
+++ src/main/java/com/nuracell/shelltest/command/MyCommands.java
@@ -0,0 +1,21 @@
+package com.nuracell.shelltest.command;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+@ShellComponent
+public class MyCommands {
+
+    // 1.
+    // shell:>hello-world
+    // Hello world spring
+
+    // 2.
+    // shell:>hello-world --arg ArG
+    // Hello world ArG
+    @ShellMethod(key = "hello-world")
+    public String helloWorld(@ShellOption(defaultValue = "spring") String arg) {
+        return "Hello world " + arg;
+    }
+}
