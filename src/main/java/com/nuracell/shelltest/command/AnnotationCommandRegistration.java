a81f0a2ac56b70ef6a34c997311bf2103d86e166
Qwww Rere <qwertylollel@gmail.com>
2023-06-04 13:42:24 +0600

    LegacyAnnotationCommandRegistration
    Took 8 minutes

--- src/main/java/com/nuracell/shelltest/command/AnnotationCommandRegistration.java
+++ src/main/java/com/nuracell/shelltest/command/AnnotationCommandRegistration.java
@@ -1,8 +1,6 @@
 package com.nuracell.shelltest.command;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.shell.command.annotation.Command;
-import org.springframework.stereotype.Component;
 
 @Command(command = "acr", group = "Test")
 public class AnnotationCommandRegistration {
--- src/main/java/com/nuracell/shelltest/command/LegacyAnnotationCommandRegistration.java
+++ src/main/java/com/nuracell/shelltest/command/LegacyAnnotationCommandRegistration.java
@@ -1,4 +1,18 @@
 package com.nuracell.shelltest.command;
 
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
 public class LegacyAnnotationCommandRegistration {
+
+    @ShellMethod(value = "Description. Description should be no more  than one or two sentences.")
+    public void myCommand() {
+
+    }
+
+    @ShellMethod(value = "Add numbers.", key = "sum")
+    public int add(int a, int b){
+        return a + b;
+    }
 }
